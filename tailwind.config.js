93318aaa0d7d9c9e784f813f071608ededde451e
Shreyans <spgandhi@live.com>
2022-03-30 13:34:21 -0700

    add fonts and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      neutral: {
+        100: '#1E272E',
+        70: '#2B343B',
+        40: '#384047',
+        20: '#ABAEB0',
+      },
+      primary: {
+        DEFAULT: '#7B68EE',
+        40: '#7B68EE',
+      },
+      white: '#fff',
+    },
+    fontFamily: {
+      sans: ['GothamPro'],
+    },
     extend: {},
   },
   plugins: [],
